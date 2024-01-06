7b127d85da4cba0c24c246b9c201e0d748f0b825
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2024-01-06 23:54:24 +0100

    :recycle: Extract ScarfPlotHeader component

---
+++ src/lib/components/Plot/ScarfPlot/ScarfPlotHeader/ScarfPlotHeader.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import ZoomInButton from '../ScarfPlotButton/ScarfPlotButtonZoomIn.svelte'
+  import ZoomOutButton from '../ScarfPlotButton/ScarfPlotButtonZoomOut.svelte'
+  import ScarfPlotSelectStimulus from '../ScarfPlotSelect/ScarfPlotSelectStimulus.svelte'
+  import ScarfPlotButtonMenu from '../ScarfPlotButton/ScarfPlotButtonMenu.svelte'
+  import ScarfTimelineSelect from '../ScarfPlotSelect/ScarfPlotSelectTimeline.svelte'
+  import ScarfPlotSelectGroup from '../ScarfPlotSelect/ScarfPlotSelectGroup.svelte'
+
+  export let scarfId: number
+</script>
+
+<div class="nav">
+  <ScarfPlotSelectStimulus {scarfId} />
+  <ScarfTimelineSelect {scarfId} />
+  <ScarfPlotSelectGroup {scarfId} />
+  <ZoomInButton {scarfId} />
+  <ZoomOutButton {scarfId} />
+  <ScarfPlotButtonMenu {scarfId} />
+</div>
+
+<style>
+  .nav {
+    display: flex;
+    gap: 5px;
+    flex-wrap: wrap;
+  }
+</style>
