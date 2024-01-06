ae0512db96addc0cdb77b9ca6ce15fbd24e5402a
misavojte <mich.vojtechovska@gmail.com>
2024-01-06 12:23:00 +0100

    🎨 Change of code formatting - update ESLint, Prettier, Svelte, and Vite configurations

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-    semi: false, // Standard JS Style does not use semicolons
-    trailingComma: 'es5', // Trailing commas where valid in ES5 (objects, arrays, etc.)
-    singleQuote: true, // Use single quotes instead of double quotes
-    printWidth: 80, // Line length of 80 characters
-    tabWidth: 2, // Set a tab width of 2 spaces
-    useTabs: false, // Use spaces instead of tabs
-    bracketSpacing: true, // Spaces between brackets in object literals
-    arrowParens: 'avoid', // Only add parens when necessary
-    endOfLine: 'lf', // Line feed only (\n), common on Linux and macOS as well as inside git repos
-    plugins: ["prettier-plugin-svelte"],
-};
+  semi: false, // Standard JS Style does not use semicolons
+  trailingComma: 'es5', // Trailing commas where valid in ES5 (objects, arrays, etc.)
+  singleQuote: true, // Use single quotes instead of double quotes
+  printWidth: 80, // Line length of 80 characters
+  tabWidth: 2, // Set a tab width of 2 spaces
+  useTabs: false, // Use spaces instead of tabs
+  bracketSpacing: true, // Spaces between brackets in object literals
+  arrowParens: 'avoid', // Only add parens when necessary
+  endOfLine: 'lf', // Line feed only (\n), common on Linux and macOS as well as inside git repos
+  plugins: ['prettier-plugin-svelte'],
+}
