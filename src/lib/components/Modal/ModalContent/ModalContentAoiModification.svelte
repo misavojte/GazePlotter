97973ca3298cf53c8e76cfd085be17551f4feab2
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2024-01-07 01:50:16 +0100

    :lipstick: Replace unicode arrows with icons

--- src/lib/components/Modal/ModalContent/ModalContentAoiModification.svelte
+++ src/lib/components/Modal/ModalContent/ModalContentAoiModification.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
-  import type { ExtendedInterpretedDataType } from '$lib/type/Data/InterpretedData/ExtendedInterpretedDataType.ts'
-  import { getAois, getStimuli, updateMultipleAoi } from '$lib/stores/dataStore.ts'
-  import GeneralSelectBase from '$lib/components/General/GeneralSelect/GeneralSelectBase.svelte'
-  import GeneralButtonMinor from '$lib/components/General/GeneralButton/GeneralButtonMinor.svelte'
-  import { flip } from 'svelte/animate'
-  import GeneralRadio from '$lib/components/General/GeneralRadio/GeneralRadio.svelte'
   import GeneralButtonMajor from '$lib/components/General/GeneralButton/GeneralButtonMajor.svelte'
-  import { addErrorToast, addInfoToast, addSuccessToast } from '$lib/stores/toastStore.ts'
+  import GeneralButtonMinor from '$lib/components/General/GeneralButton/GeneralButtonMinor.svelte'
+  import GeneralRadio from '$lib/components/General/GeneralRadio/GeneralRadio.svelte'
+  import GeneralSelectBase from '$lib/components/General/GeneralSelect/GeneralSelectBase.svelte'
+  import {
+    getAois,
+    getStimuli,
+    updateMultipleAoi,
+  } from '$lib/stores/dataStore.ts'
+  import {
+    addErrorToast,
+    addInfoToast,
+    addSuccessToast,
+  } from '$lib/stores/toastStore.ts'
+  import type { ExtendedInterpretedDataType } from '$lib/type/Data/InterpretedData/ExtendedInterpretedDataType.ts'
+  import ArrowDown from 'lucide-svelte/icons/arrow-down'
+  import ArrowUp from 'lucide-svelte/icons/arrow-up'
+  import { onMount } from 'svelte'
+  import { flip } from 'svelte/animate'
 
   export let selectedStimulus = '0'
   export let userSelected = 'this'
-  let aoiObjects: ExtendedInterpretedDataType[] = [] /* Order in array is also important */
+  let aoiObjects: ExtendedInterpretedDataType[] =
+    [] /* Order in array is also important */
 
   $: {
     aoiObjects = getAois(parseInt(selectedStimulus))
@@ -20,10 +31,10 @@
   /**
    * TODO: Make reactive in the future (when stimuli can be updated)
    */
-  const stimuliOption = getStimuli().map((stimulus) => {
+  const stimuliOption = getStimuli().map(stimulus => {
     return {
       label: stimulus.displayedName,
-      value: stimulus.id.toString()
+      value: stimulus.id.toString(),
     }
   })
 
@@ -39,7 +50,7 @@
         ...aoiObjects.slice(0, index - 1),
         aoiObjects[index],
         aoiObjects[index - 1],
-        ...aoiObjects.slice(index + 1)
+        ...aoiObjects.slice(index + 1),
       ]
     }
   }
@@ -51,17 +62,26 @@
         ...aoiObjects.slice(0, index),
         aoiObjects[index + 1],
         aoiObjects[index],
-        ...aoiObjects.slice(index + 2)
+        ...aoiObjects.slice(index + 2),
       ]
     }
   }
 
   const handleSubmit = () => {
-    let handlerTypeForAoiStore: 'this_stimulus' | 'all_by_original_name' | 'all_by_displayed_name' = 'this_stimulus'
-    if (userSelected === 'all_original') handlerTypeForAoiStore = 'all_by_original_name'
-    if (userSelected === 'all_displayed') handlerTypeForAoiStore = 'all_by_displayed_name'
+    let handlerTypeForAoiStore:
+      | 'this_stimulus'
+      | 'all_by_original_name'
+      | 'all_by_displayed_name' = 'this_stimulus'
+    if (userSelected === 'all_original')
+      handlerTypeForAoiStore = 'all_by_original_name'
+    if (userSelected === 'all_displayed')
+      handlerTypeForAoiStore = 'all_by_displayed_name'
     try {
-      updateMultipleAoi(aoiObjects, parseInt(selectedStimulus), handlerTypeForAoiStore)
+      updateMultipleAoi(
+        aoiObjects,
+        parseInt(selectedStimulus),
+        handlerTypeForAoiStore
+      )
     } catch (e) {
       console.error(e)
       addErrorToast('Error while updating AOIs. See console for more details.')
@@ -71,104 +91,115 @@
       addInfoToast('Ordering of AOIs is not updated for other stimuli')
     }
   }
-
 </script>
 
 <div class="content">
-    <GeneralSelectBase
-        label="For stimulus"
-        options={stimuliOption}
-        bind:value={selectedStimulus}
-    />
+  <GeneralSelectBase
+    label="For stimulus"
+    options={stimuliOption}
+    bind:value={selectedStimulus}
+  />
 </div>
-{#if (aoiObjects.length === 0)}
-    <div>No AOIs found</div>
+{#if aoiObjects.length === 0}
+  <div>No AOIs found</div>
 {/if}
-{#if (aoiObjects.length > 0)}
-    <table class="grid content">
-        <thead>
-        <tr class='gr-line header'>
-            <th>Name</th>
-            <th>Displayed name</th>
-            <th>Color</th>
-            <th>Order</th>
+{#if aoiObjects.length > 0}
+  <table class="grid content">
+    <thead>
+      <tr class="gr-line header">
+        <th>Name</th>
+        <th>Displayed name</th>
+        <th>Color</th>
+        <th>Order</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each aoiObjects as aoi (aoi.id)}
+        <tr class="gr-line" animate:flip>
+          <td class="original-name">{aoi.originalName}</td>
+          <td>
+            <input
+              type="text"
+              id={aoi.id + 'displayedName'}
+              bind:value={aoi.displayedName}
+            />
+          </td>
+          <td>
+            <input type="color" id={aoi.id + 'color'} bind:value={aoi.color} />
+          </td>
+          <td>
+            <div class="button-group">
+              <GeneralButtonMinor
+                isDisabled={aoiObjects.indexOf(aoi) === 0}
+                on:click={() => handleObjectPositionUp(aoi)}
+              >
+                <ArrowUp size={'1em'} />
+              </GeneralButtonMinor>
+              <GeneralButtonMinor
+                isDisabled={aoiObjects.indexOf(aoi) === aoiObjects.length - 1}
+                on:click={() => handleObjectPositionDown(aoi)}
+              >
+                <ArrowDown size={'1em'} />
+              </GeneralButtonMinor>
+            </div>
+          </td>
         </tr>
-        </thead>
-        <tbody>
-        {#each aoiObjects as aoi (aoi.id)}
-            <tr class='gr-line' animate:flip>
-                <td class="original-name">{aoi.originalName}</td>
-                <td>
-                    <input type="text" id={aoi.id + 'displayedName'} bind:value={aoi.displayedName} />
-                </td>
-                <td>
-                    <input type="color" id={aoi.id + 'color'} bind:value={aoi.color} />
-                </td>
-                <td>
-                    <div class="button-group">
-                        <GeneralButtonMinor isDisabled={aoiObjects.indexOf(aoi) === 0} marginTop="0" on:click={() => handleObjectPositionUp(aoi)}>↑</GeneralButtonMinor>
-                        <GeneralButtonMinor isDisabled={aoiObjects.indexOf(aoi) === aoiObjects.length - 1} marginTop="0" on:click={() => handleObjectPositionDown(aoi)}>↓</GeneralButtonMinor>
-                    </div>
-                </td>
-            </tr>
-        {/each}
-        </tbody>
-    </table>
-    <div class="content">
-        <GeneralRadio
-                legend="Apply changes to"
-                options={[
-                  { label: 'This stimulus', value: 'this' },
-                  { label: 'All by original name', value: 'all_original' },
-                  { label: 'All by displayed name', value: 'all_displayed' }
-                ]}
-                bind:userSelected
-        />
-    </div>
-    <GeneralButtonMajor on:click={handleSubmit}>
-        Apply
-    </GeneralButtonMajor>
+      {/each}
+    </tbody>
+  </table>
+  <div class="content">
+    <GeneralRadio
+      legend="Apply changes to"
+      options={[
+        { label: 'This stimulus', value: 'this' },
+        { label: 'All by original name', value: 'all_original' },
+        { label: 'All by displayed name', value: 'all_displayed' },
+      ]}
+      bind:userSelected
+    />
+  </div>
+  <GeneralButtonMajor on:click={handleSubmit}>Apply</GeneralButtonMajor>
 {/if}
 
 <style>
-    /* Component Group */
-    input {
-        height: 34px;
-        box-sizing: border-box;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        margin: 0;
-        padding: 0.5rem;
-    }
+  /* Component Group */
+  input {
+    height: 34px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    margin: 0;
+    padding: 0.5rem;
+  }
 
-    input[type="color"] {
-        height: 34px;
-        width: 50px;
-        padding: 4px;
-        background: none;
-    }
+  input[type='color'] {
+    height: 34px;
+    width: 50px;
+    padding: 4px;
+    background: none;
+  }
 
-    .button-group {
-        display: flex;
-        gap: 5px;
-        flex-direction: row;
-    }
+  .button-group {
+    display: flex;
+    gap: 5px;
+    flex-direction: row;
+  }
 
-    th {
-        text-align: left;
-        font-size: 14px;
-    }
-    .original-name {
-        font-size: 14px;
-        padding-right: 10px;
-        color: #999;
-        cursor: not-allowed;
-    }
-    .content {
-        margin-bottom: 25px;
-    }
-    .original-name {
-        line-height: 1;
-        white-space: nowrap;
-    }
+  th {
+    text-align: left;
+    font-size: 14px;
+  }
+  .original-name {
+    font-size: 14px;
+    padding-right: 10px;
+    color: #999;
+    cursor: not-allowed;
+  }
+  .content {
+    margin-bottom: 25px;
+  }
+  .original-name {
+    line-height: 1;
+    white-space: nowrap;
+  }
 </style>
