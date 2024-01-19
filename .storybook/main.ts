6a777f8e548039cb66de1c39b0bc1aabb8ec6364
Jakob Listabarth <jakob@listabarth.at>
2024-01-19 16:17:25 +0100

    :heavy_plus_sign: Add storybook addon svelte-csf
    * :arrow_up: Upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,14 @@
 import type { StorybookConfig } from '@storybook/sveltekit'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx|svelte)'],
   staticDirs: ['../static'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-svelte-csf',
   ],
   framework: {
     name: '@storybook/sveltekit',
