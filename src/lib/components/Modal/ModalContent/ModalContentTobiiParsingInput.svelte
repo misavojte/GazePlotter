f5664a613e6382b71d2cf4f46e56eb3abca6bf20
misavojte <mich.vojtechovska@gmail.com>
2024-01-29 10:37:08 +0100

    :lipstick: Improved Modal UI - new margin bottom - better labels and information - removed redundant file

--- src/lib/components/Modal/ModalContent/ModalContentTobiiParsingInput.svelte
+++ src/lib/components/Modal/ModalContent/ModalContentTobiiParsingInput.svelte
@@ -11,22 +11,33 @@
   })
 </script>
 
-<p>Tobii Pro Lab data with Event column detected. How to parse stimuli?</p>
-<GeneralRadio
-  options={[
-    { value: '', label: 'Default by stimulus name' },
-    {
-      value: 'IntervalStart;IntervalEnd',
-      label: `By event's '%NAME% IntervalStart' and '%NAME% IntervalEnd'`,
-    },
-    {
-      value: '_start;_end',
-      label: `By event's '%NAME%_start' and '%NAME%_end'`,
-    },
-  ]}
-  legend="Stimuli parsing"
-  bind:userSelected={value}
-/>
+<div class="content">
+  <p>
+    GazePlotter detected Tobii Pro Lab data with the Event column. How should
+    the stimulus be determined?
+  </p>
+  <GeneralRadio
+    options={[
+      { value: '', label: 'Media record column' },
+      {
+        value: 'IntervalStart;IntervalEnd',
+        label: `Event's '%NAME% IntervalStart' and '%NAME% IntervalEnd'`,
+      },
+      {
+        value: '_start;_end',
+        label: `Event's '%NAME%_start' and '%NAME%_end'`,
+      },
+    ]}
+    legend="Stimulus will be determined by"
+    bind:userSelected={value}
+  />
+</div>
 <GeneralButtonMajor on:click={() => valuePromiseResolve(value)}>
   Apply
 </GeneralButtonMajor>
+
+<style>
+  .content {
+    margin-bottom: 2rem;
+  }
+</style>
