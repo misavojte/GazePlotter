d955d63ea675aaecb3c4bf8d17adc912c94d0b5d
misavojte <mich.vojtechovska@gmail.com>
2024-02-05 09:34:22 +0100

    :recycle: Removed unused files

--- src/lib/class/AoiVisibilityParser/AoiVisibilityParser.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class AoiVisibilityParser {
-    addVisInfo(stimulusId: number, participantId: number | null, xml: Document): void;
-    processKeyFrames(keyFrames: HTMLCollectionOf<Element>, stimulusId: number): number[];
-}
