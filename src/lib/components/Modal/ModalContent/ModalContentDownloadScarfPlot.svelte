4233aae096567a62ecffec6525e66380c5b75af3
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2025-04-28 09:58:45 +0200

    Update import path for ScarfFillingType to improve project structure
    This commit modifies the import path for the ScarfFillingType from the type directory to the plots directory, aligning with recent organizational changes in the codebase. This adjustment enhances clarity and maintainability of the component imports.

--- src/lib/components/Modal/ModalContent/ModalContentDownloadScarfPlot.svelte
+++ src/lib/components/Modal/ModalContent/ModalContentDownloadScarfPlot.svelte
@@ -2,7 +2,7 @@
   import DownloadPlotSettings from '$lib/components/Modal/Shared/DownloadPlotSettings.svelte'
   import type { ScarfGridType } from '$lib/type/gridType'
   import ScarfPlotFigure from '$lib/plots/scarf/components/ScarfPlotFigure.svelte'
-  import type { ScarfFillingType } from '$lib/type/Filling/ScarfFilling/ScarfFillingType'
+  import type { ScarfFillingType } from '$lib/plots/scarf/types/ScarfFillingType'
   import GeneralCanvasPreview from '$lib/components/General/GeneralCanvasPreview/GeneralCanvasPreview.svelte'
   import { transformDataToScarfPlot } from '$lib/plots/scarf/utils/transformations'
   import { getParticipants } from '$lib/stores/dataStore'
