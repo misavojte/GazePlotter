3e82cef2128f790521179d80f39a02d7c5f9f5b2
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2025-11-04 08:42:46 +0200

    basis for context-menu

---
+++ src/lib/context-menu/index.ts
@@ -0,0 +1,6 @@
+export { contextMenuAction } from './components/contextMenuAction'
+export type { ContextMenuOptions } from './components/contextMenuAction'
+export type { MenuItem } from './components/contextMenuAction'
+export { contextMenuStore, updateContextMenu } from './stores'
+
+
